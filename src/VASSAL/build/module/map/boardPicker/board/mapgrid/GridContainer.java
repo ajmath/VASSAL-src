2d35c4aff2fc51ab239ab9b571cd00d1df275a6b
Andrew Matheny <andrew.j.matheny@gmail.com>
2017-07-13 00:55:06 -0400

    Crappy git svn initial commit

---
+++ src/VASSAL/build/module/map/boardPicker/board/mapgrid/GridContainer.java
@@ -0,0 +1,36 @@
+/*
+ * $Id: GridContainer.java 8359 2012-09-18 19:58:04Z uckelman $
+ *
+ * Copyright (c) 2004-2012 by Rodney Kinney, Brent Easton
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License (LGPL) as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, copies are available
+ * at http://www.opensource.org.
+ */
+package VASSAL.build.module.map.boardPicker.board.mapgrid;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+import VASSAL.build.module.map.boardPicker.Board;
+import VASSAL.build.module.map.boardPicker.board.MapGrid;
+
+/**
+ * A Component that can contain a {@link VASSAL.build.module.map.boardPicker.board.MapGrid}
+ */
+public interface GridContainer {
+  void setGrid(MapGrid grid);
+  void removeGrid(MapGrid grid);
+  Board getBoard();
+  Dimension getSize();
+  boolean contains(Point point);
+}
